f4ea1d814fb7f9c061930dfda2dc96829f7057ca
veronaten <verona.binko@gmail.com>
2021-01-10 00:47:16 +0300

    fix gaps in script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// Мое решение:
-function pow(x,n) {
-    let mult = x;
-    for (let i = 1; i < n; i++ ) {
-        mult *= x;
-    }
 
-    return mult;
+function pow(x, n) {
+  let mult = x;
+  for (let i = 1; i < n; i++ ) {
+      mult *= x;
+  }
+
+  return mult;
 }
     
 const x = Number(prompt (`Введите число х`, '1'));
 const n = Number(prompt (`Введите число n`, '1'));
-const result = pow(x,n);
+const result = pow(x, n);
     
 if (n >= 1) {
-    alert (result);
+  alert(result);
 } else {
-    alert (`n должно быть целым и больше 1`);
+  alert(`n должно быть целым и больше 1`);
 }
     
   
