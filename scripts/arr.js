549d45681f6743a29f9fcd623d00770cd0d9c6ee
veronaten <verona.binko@gmail.com>
2021-01-18 01:43:42 +0300

    add function arr.js task 1, 2 delete class number and choose

--- scripts/arr.js
+++ scripts/arr.js
@@ -1,18 +1,33 @@
+function sumMultiply(array) {
+  if (!Array.isArray(array) || array.lenth !==4) {
+    return 'Array is expected to have 4 numbers';
+  }
 
-let arr = [2, 5, 3, 9];
-let result = (arr[0]*arr[1]) + (arr[2]*arr[3]);
-alert(result);
+  return array[0] * array[1] + array[2] * array [3];
+}
+function runSumMultiply() {
+  const userArray = prompt('Enter array:', '[2, 5, 3, 9]') || [];
+
+  const result = sumMultiply(userArray);
+
+  alert(result);
+} 
+
+function dayNumber(week) {
+  const week = {1: 'Пн', 2: "Вт", 3: "Ср", 4: "Чт", 5: "Пт", 6: "Сб", 7: "Вскр"};
+  return week[userDay];
+}
+function runDayNumber() {
+  const userDay = Number(prompt('Введите число дня недели', [3]));
+  const show = dayNumber(userDay);
+  alert(show);
+}
 
 
-let week = {1: 'Пн', 2: "Вт", 3: "Ср", 4: "Чт", 5: "Пт", 6: "Сб", 7: "Вскр"};
-let day = 3;
-alert(week[`${day}`]);
-
-
-let lang = 'ru' || 'en';
-let day = 5;
-let week = {
-  'ru': ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вскр"],
-  'en': ["M", "T", "W", "Th", "Fr", "St", "Sd"],
-};
-alert(week[lang][day]);
+// let lang = 'ru' || 'en';
+// let day = 5;
+// let week = {
+//   'ru': ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вскр"],
+//   'en': ["M", "T", "W", "Th", "Fr", "St", "Sd"],
+// };
+// alert(week[lang][day]);
--- scripts/pow.js
+++ scripts/pow.js
@@ -19,9 +19,3 @@ function runPow() {
       alert (`n должно быть целым и больше 1`);
   }
 }
-
-
-
-let arr = [2, 5, 3, 9];
-let result = (arr[0]*arr[1]) + (arr[2]*arr[3]);
-alert(result);
