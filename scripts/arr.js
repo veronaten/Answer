30440da295fdc0237d077bddcd61c774595d33bb
veronaten <verona.binko@gmail.com>
2021-01-18 20:37:16 +0300

    correct second task, add function third task

--- scripts/arr.js
+++ scripts/arr.js
@@ -13,21 +13,48 @@ function runSumMultiply() {
   alert(result);
 } 
 
-function dayNumber(week) {
-  const week = {1: 'Пн', 2: "Вт", 3: "Ср", 4: "Чт", 5: "Пт", 6: "Сб", 7: "Вскр"};
-  return week[userDay];
-}
+const week = { 1: 'Пн', 2: 'Вт', 3: 'Ср', 4: 'Чт', 5: 'Пт', 6: 'Сб', 7: 'Вскр' }
+
 function runDayNumber() {
-  const userDay = Number(prompt('Введите число дня недели', [3]));
-  const show = dayNumber(userDay);
-  alert(show);
+  const userDayNumber = Number(prompt('Введите число дня недели', 3) || 0);
+
+  if (Number.isNaN(userDayNumber)) {
+    alert('Enter valid number please')
+
+    return
+  }
+
+  const dayName = week[userDayNumber];
+
+  alert(`Here's your day: ${dayName}`);
 }
 
+const weekDay = { 1: 'Md', 2: 'Ts', 3: 'W', 4: 'Th', 5: 'Fr', 6: 'St', 7: 'Sd'};
+    
+function chooseLang() {
+    const userLang = prompt('Выберите язык: "ru" или "eng"', ru);
 
-// let lang = 'ru' || 'en';
-// let day = 5;
-// let week = {
-//   'ru': ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вскр"],
-//   'en': ["M", "T", "W", "Th", "Fr", "St", "Sd"],
-// };
-// alert(week[lang][day]);
+    if (userLang === ru) {
+        runDayNumber();
+    } else if (userLang === eng) {
+        runDayChoose();
+    } else {
+        alert('Выберите язык и введите "ru" или "eng"');
+    }
+
+    return
+}
+
+function runDayChoose() {
+  const userDayNumber = Number(prompt('Введите число дня недели', 3) || 0);
+
+  if (Number.isNaN(userDayNumber)) {
+    alert('Enter valid number please')
+
+    return
+  }
+
+  const dayName = weekDay[userDayNumber];
+
+  alert(`Here's your day: ${dayName}`);
+}
