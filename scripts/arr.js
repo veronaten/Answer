cf11ee4cae7bb7b8acd0571515535d3199261027
veronaten <verona.binko@gmail.com>
2021-01-20 23:49:14 +0300

    Change language from Ru to Eng in arr.js, add if.js

--- scripts/arr.js
+++ scripts/arr.js
@@ -19,7 +19,7 @@ function runSumMultiply() {
 const week = { 1: 'Пн', 2: 'Вт', 3: 'Ср', 4: 'Чт', 5: 'Пт', 6: 'Сб', 7: 'Вскр' }
 
 function runDayNumber() {
-  const userDayNumber = Number(prompt('Введите число дня недели', 3) || 0);
+  const userDayNumber = Number(prompt('Enter the number of the day of the week', 3) || 0);
 
   if (Number.isNaN(userDayNumber)) {
     alert('Enter valid number please')
@@ -35,21 +35,20 @@ function runDayNumber() {
 const weekDay = { 1: 'Md', 2: 'Ts', 3: 'W', 4: 'Th', 5: 'Fr', 6: 'St', 7: 'Sd'};
     
 function chooseLang() {
-    const userLang = prompt('Выберите язык: "ru" или "eng"', 'ru');
+    const userLang = prompt('Select a language: "ru" or "eng"', 'ru');
 
     if (userLang === 'ru') {
         runDayNumber();
     } else if (userLang === 'eng') {
         runDayChoose();
     } else {
-        alert('Выберите язык и введите "ru" или "eng"');
+        alert('Select a language and enter "ru" or "eng"');
     }
-
     return
 }
 
 function runDayChoose() {
-  const userDayNumber = Number(prompt('Введите число дня недели', 3) || 0);
+  const userDayNumber = Number(prompt('Enter the number of the day of the week', 3) || 0);
 
   if (Number.isNaN(userDayNumber)) {
     alert('Enter valid number please')
---
+++ scripts/if.js
@@ -0,0 +1,22 @@
+let a = '1';
+if(a === '1') {
+  alert('Верно');
+} else {
+  alert('Неверно');
+}
+
+
+
+let test = false;
+if(test !== true){
+  alert('Верно');
+} else {
+  alert('Неверно');
+}
+
+let test = true;
+if(!test){
+  alert('Верно');
+} else {
+  alert('Неверно');
+}
