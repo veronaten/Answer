1514e60459596b2af2af9f3515fefeaeb6b1653a
veronaten <verona.binko@gmail.com>
2021-01-21 23:59:05 +0300

    Change task 3, combine 3 function into 1, in .html add <meta>

--- scripts/arr.js
+++ scripts/arr.js
@@ -32,22 +32,9 @@ function runDayNumber() {
   alert(`Here's your day: ${dayName}`);
 }
 
-const weekDay = { 1: 'Md', 2: 'Ts', 3: 'W', 4: 'Th', 5: 'Fr', 6: 'St', 7: 'Sd'};
-    
-function chooseLang() {
-    const userLang = prompt('Select a language: "ru" or "eng"', 'ru');
+const weekDay = {1: 'Mn', 2: 'Ts', 3: 'Wd', 4: 'Th', 5: 'Fr', 6: 'St', 7: 'Sd' } 
 
-    if (userLang === 'ru') {
-        runDayNumber();
-    } else if (userLang === 'eng') {
-        runDayChoose();
-    } else {
-        alert('Select a language and enter "ru" or "eng"');
-    }
-    return
-}
-
-function runDayChoose() {
+function dayChoose() {
   const userDayNumber = Number(prompt('Enter the number of the day of the week', 3) || 0);
 
   if (Number.isNaN(userDayNumber)) {
@@ -56,7 +43,20 @@ function runDayChoose() {
     return
   }
 
+  const dayNumber = week[userDayNumber];
+
   const dayName = weekDay[userDayNumber];
 
-  alert(`Here's your day: ${dayName}`);
+  const userLang = prompt('Select a language: "ru" or "eng"', 'ru');
+
+  if(userLang === 'ru') {
+    alert(`Here's your day: ${dayNumber}`);
+  } else if (userLang === 'eng') {
+    alert(`Here's your day: ${dayName}`);
+  } else {
+    alert('Select a language and enter "ru" or "eng"');
+  }
 }
+
+
+
