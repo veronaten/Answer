bdcae8ad4dbdf6c257f2de146d1546a285f0dffb
veronaten <verona.binko@gmail.com>
2021-01-25 23:39:28 +0300

    File if.js change function getTimeOfYear with arroy

--- scripts/if.js
+++ scripts/if.js
@@ -1,27 +1,15 @@
-function timeOfYear() {
-  const userNum = Number(prompt('Enter a number from 1 to 4', 1) || 0);
+const year = ['winter', 'spring', 'summer', 'autumn'];
+
+function getTimeOfYear() {
+  const userNumber = Number(prompt('Enter a number from 1 to 4', 1) || 0);
       
-  if (userNum < 1 && userNum > 4) {
+  if (userNumber < 1 && userNumber > 4) {
     alert('Enter valid number from 1 to 4 please');
     
     return;
   }
-    
-  switch (userNum) {
-    case 1:
-      alert('It is summer!');
-    break;
-    case 2:
-      alert('It is autumn!');
-    break;
-    case 3:
-      alert('It is winter!');
-    break;
-    case 4:
-      alert('It is spring!');
-    break;
-    default:
-      alert('Enter valid number from 1 to 4 please');
-    break;
-  }
+
+  const timeOfYear = year[userNumber - 1];
+
+  alert(`It is ${timeOfYear}!`);
 }
