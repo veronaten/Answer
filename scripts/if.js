8edbf6122c3f243f1cfd6f2fea03945beb8ca867
veronaten <verona.binko@gmail.com>
2021-01-27 00:25:41 +0300

    Use arroy seasons for 2 functions. The second task make through if

--- scripts/if.js
+++ scripts/if.js
@@ -1,44 +1,46 @@
-const yearMonths = ['winter', 'spring', 'summer', 'autumn'];
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
 function getTimeOfYear() {
   const userNumber = Number(prompt('Enter a number from 1 to 4', 1) || 0);
        
-  if (userNumber < 1 && userNumber > 4) {
+  if (userNumber < 1 || userNumber > 4) {
     alert('Enter valid number from 1 to 4 please');
     
     return;
   }
 
-  const timeOfYear = yearMonths[userNumber - 1];
-
   switch (userNumber) {
     case 1:
-      alert(`It is ${timeOfYear}!`); 
+      alert(`It is ${seasons[0]}!`); 
     break;
     case 2:
-      alert(`It is ${timeOfYear}!`);
+      alert(`It is ${seasons[1]}!`);
     break;
     case 3:
-      alert(`It is ${timeOfYear}!`);
+      alert(`It is ${seasons[2]}!`);
     break;
     case 4:
-      alert(`It is ${timeOfYear}!`);
+      alert(`It is ${seasons[3]}!`);
     break;
   }
 }
 
-const months = {1: 'winter', 2: 'winter', 3: 'winter', 4: 'spring', 5: 'spring', 6: 'spring', 7: 'summer', 8: 'summer', 9: 'summer', 10: 'autumn', 11: 'autumn', 12: 'autumn'};
-
 function getMonth() {
   const userMonth = Number(prompt('Enter a number of month from 1 to 12', 1) || 0);
 
-  if (userMonth < 1 && userMonth > 12) {
+  if (userMonth < 1 || userMonth > 12) {
     alert('Enter the month number please, from 1 to 12');
 
     return;
   }
 
-  const result = months[userMonth];
-
-  alert(`Your time of year is ${result}.`);
+  if (userMonth == 1 && userMonth < 4) {
+    alert(`It is ${seasons[0]}`); 
+  } else if (userMonth >= 4 && userMonth < 7) {
+    alert(`It is ${seasons[1]}`);
+  } else if (userMonth >=7 && userMonth < 10) {
+    alert(`It is ${seasons[2]}`);
+  } else if (userMonth >= 10 && userMonth <= 12) {
+    alert(`It is ${seasons[3]}`);
+  }
 }
