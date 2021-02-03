bf4ed43dfa76a129fb4fe416ef26e736a88893d1
veronaten <verona.binko@gmail.com>
2021-02-04 01:30:59 +0300

    Make new function chooseWeekDays, first task

--- scripts/cycles.js
+++ scripts/cycles.js
@@ -4,7 +4,7 @@ function chooseWeekDays() {
   let chooseDay = Number(prompt('Enter number today day jf the week, from 1 to 7', '1') || 0);
 
   if (chooseDay < 1 || chooseDay > 7 || Number.isNaN(chooseDay)) {
-    alert('Enter valid number, please, from 1 to 7.');
+    alert('Enter valid number, please, from 1 to 7');
 
     return;
   }
