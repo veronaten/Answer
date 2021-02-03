762b3337a928ed9b1f01463838dbe2c410d7187c
veronaten <verona.binko@gmail.com>
2021-02-04 01:24:03 +0300

    Add new file cycles.js

---
+++ scripts/cycles.js
@@ -0,0 +1,19 @@
+const week = ['Md', 'Ts', 'Ws', 'Th', 'Fr', 'St', 'Sd'];
+
+function chooseWeekDays() {
+  let chooseDay = Number(prompt('Enter number today day jf the week, from 1 to 7', '1') || 0);
+
+  if (chooseDay < 1 || chooseDay > 7 || Number.isNaN(chooseDay)) {
+    alert('Enter valid number, please, from 1 to 7.');
+
+    return;
+  }
+
+  for (let day = 0; day < week.length; day++) {
+    if (day === chooseDay - 1) {
+      document.write(week[day].italics());
+    } else {
+      document.write(week[day]);
+    }
+  }
+}
