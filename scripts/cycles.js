831215a73d7381d4b62692d9508847e52e60a928
veronaten <verona.binko@gmail.com>
2021-02-04 14:04:07 +0300

    Change let chooseDay on const chooseDay

--- scripts/cycles.js
+++ scripts/cycles.js
@@ -1,7 +1,7 @@
 const week = ['Md', 'Ts', 'Ws', 'Th', 'Fr', 'St', 'Sd'];
 
 function chooseWeekDays() {
-  let chooseDay = Number(prompt('Enter number today day jf the week, from 1 to 7', '1') || 0);
+  const chooseDay = Number(prompt('Enter number today day jf the week, from 1 to 7', '1') || 0);
 
   if (chooseDay < 1 || chooseDay > 7 || Number.isNaN(chooseDay)) {
     alert('Enter valid number, please, from 1 to 7');
