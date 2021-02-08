d94a6f41df33832d42dc76b89d18222cc6fdb08f
veronaten <verona.binko@gmail.com>
2021-02-08 15:20:23 +0300

    Delete function runArithmetic, change functions findMean and findFactorial

--- scripts/math.js
+++ scripts/math.js
@@ -1,27 +1,23 @@
-function runArithmetic(array) {
-  if (!Array.isArray(array) || array.length < 2) {
-    alert('Enter minimum 2 numbers separated by space, please');
-  }
-
-  return array[i] / 2;
-}
-
 function findMean() {
   const userNumbers = prompt('Enter some integer numbers separated by spaces', '12 15 20 25 59 79') || '0';
 
   const array = userNumbers.split(' ');
 
+  if (!Array.isArray(array) || array.length < 2) {
+    alert('Enter minimum 2 numbers separated by space, please');
+  }
+
   let result = 0;
 
-  for (i = 0; i < array.length; i++) {
-    result = result + runArithmetic(array);
+  for (let i = 0; i < array.length; i++) {
+    result = result + array[i] / 2;
   }
 
   alert(`The arithmetic mean of your array is ${result}.`);
 }
 
 function findFactorial() {
-  let inputNumber = Number(prompt('Enter a number', '5') || 0);
+  const inputNumber = Number(prompt('Enter a number', '5') || 0);
 
   if (Number.isNaN(inputNumber)) {
     alert('Enter a number, please.');
@@ -32,10 +28,10 @@ function findFactorial() {
   let result = 1;
 
   for (let i = 1; i < inputNumber; i++) {
-    result = result * i;
+    result *= i;
   }
   
-  let factorial = result * inputNumber;
+  const factorial = result * inputNumber;
 
   alert(`The factorial ${inputNumber}! is ${factorial}`);
 }
